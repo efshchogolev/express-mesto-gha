781166b9235a47669f5f0dd4f3d4177702cc5f36
Efim Shchegolev <efimblashck@yandex.ru>
2022-10-28 20:08:50 +0300

    fix: добавить орабтку ошибок celebrate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const userRouter = require('./routes/userRoutes');
 const cardRouter = require('./routes/cardRoutes');
 const { tokenAuth } = require('./middlewares/auth');
@@ -27,6 +28,9 @@ app.use('*', (req, res) => {
     message: 'Неверный адрес',
   });
 });
+
+app.use(errors());
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
